6b5202948a1ba4bb0418decc35b693f04eff6af1
Jaime Echevarria <jaime030690@gmail.com>
2020-03-29 22:54:16 -0400

    added horizontal bar chart for all otu data

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,3 +1,5 @@
+
+
 // look at json data
 d3.json("./samples.json").then(function(data) {
     
@@ -12,12 +14,47 @@ d3.json("./samples.json").then(function(data) {
         .append("option")
         .text(d => d);
     
-    
-    
-
 });
 
 // function updates bar chart when a menu item is chosen
 function optionChanged(val) {
     console.log(val);
+    updateBarChart(val);
+}
+
+
+
+function updateBarChart(val) {
+
+    d3.json("./samples.json").then(function(data) {
+        
+        var otus = data.samples;
+
+        var otu_data = {
+            otu_ids: [],
+            sample_values: [],
+            otu_labels: []
+        };
+        
+        // loop through data to find matching name value
+        for (var i = 0; i < otus.length; i++) {
+            if (otus[i].id === val) {
+                otu_data.otu_ids = otus[i].otu_ids;
+                otu_data.sample_values = +otus[i].sample_values;
+                otu_data.otu_labels = otus[i].otu_labels;
+            }
+        };
+
+        var trace1 = {
+            x: otu_data.sample_values,
+            y: otu_data.otu_ids,
+            type: "bar",
+            orientation: "h"
+        };
+
+        var data = [trace1];
+
+        Plotly.newPlot("bar", data);
+
+    });
 }
